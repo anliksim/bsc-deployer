b26c378164bcc3c23a4bbbcabc71a3f12105b0a8
Simon Anliker <anliksim@users.noreply.github.com>
2020-04-25 13:55:00 +0200

    Add stop argument to client

--- client.sh
+++ client.sh
@@ -9,12 +9,18 @@ to_json() {
 EOF
 }
 
+case "$1" in
+"stop")
+  http_method=DELETE
+  ;;
+*)
+  http_method=POST
+  ;;
+esac
+
 headRev=$(git log --pretty=format:'%h %s' --abbrev-commit -1)
 workdir=$(dirname `pwd`)
-
 data=$(to_json "$headRev" "$workdir/bsc-env/apps")
 
-out=$(curl -k -sS -X POST "http://localhost:3557/v1/deployments" --data "$data")
-#curl -k -X DELETE "http://localhost:3557/v1/deployments" --data "$data"
+out=$(curl -k -sS -X $http_method "http://localhost:3557/v1/deployments" --data "$data")
 echo "Deployment started at $out"
-
